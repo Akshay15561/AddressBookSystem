ccee3b9b87753564ee6ee4d24e969fa54cd20f83
Akshay15561 <akshaynakawale12@gmail.com>
2024-11-26 22:35:31 +0530

    uc1-Added functionality to create and display a contact

--- src/main/java/org/example/AddressBookMain.java
+++ src/main/java/org/example/AddressBookMain.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -7,6 +8,40 @@ public class AddressBookMain {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Welcome to Address Book Program!\n");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nEnter Contact Details:");
+
+        System.out.print("First Name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Last Name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Address: ");
+        String address = scanner.nextLine();
+
+        System.out.print("City: ");
+        String city = scanner.nextLine();
+
+        System.out.print("State: ");
+        String state = scanner.nextLine();
+
+        System.out.print("ZIP: ");
+        String zip = scanner.nextLine();
+
+        System.out.print("Phone Number: ");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
+
+        System.out.println("\nContact Created:");
+        System.out.println(contact);
+
     }
 }
 
